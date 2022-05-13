33e8bc943ea6ae7350717c9d046ff95bdc2f4f77
ChamiduDev-5224 <pplchamiduravihara@gmail.com>
2022-05-13 14:58:41 +0530

    File rescources inserted to repo

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+//mongo package imported
+const mongoose = require ("mongoose");
+
+const app = express();
+
+//Mongo db connection url
+mongoose.connect("mongodb+srv://chamidu5224:Chamidu%405224@cluster0.jj0ze.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+)
+.then(()=>app.listen(5000))
+.then(()=>console.log ("Database connected")
+)
+//exception handling
+.catch((err)=>console.log("database connection error : "+ err));
+

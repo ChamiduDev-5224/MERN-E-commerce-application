9a1e1f9d508bb4ef63453f831b4bf377e920a465
ChamiduDev-5224 <pplchamiduravihara@gmail.com>
2022-05-13 23:52:05 +0530

    frontend design-header bar design

---
+++ frontend/src/components/header/Header.js
@@ -0,0 +1,13 @@
+
+import  "./header.css";
+
+const header = () => {
+ return (
+     <div>
+         <header>
+             <a href="/">AmazonZ</a>
+         </header>
+     </div>
+ );
+}
+export default header;
